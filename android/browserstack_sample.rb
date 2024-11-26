9f666c242a23f28e7ce44de71cf621e2a27cf6ce
christineroseparkers01 <christineroseparkers01@gmail.com>
2024-11-25 23:14:59 -0500

    Update browserstack_sample.rb

--- android/browserstack_sample.rb
+++ android/browserstack_sample.rb
@@ -1,54 +1,24 @@
 require 'rubygems'
 require 'appium_lib'
 require 'selenium-webdriver'
-
 caps = {}
 # Set your access credentials
-caps['browserstack.user'] = 'YOUR_USERNAME'
-caps['browserstack.key'] = 'YOUR_ACCESS_KEY'
-
+caps['browserstack.user'] = 'christinamarie_Fi3wpZ'
+caps['browserstack.key'] = 't3La1pRW7H2bqmAs4ppr'
 # Set URL of the application under test
-caps['app'] = 'bs://<app-id>'
-
+caps['app'] = 'bs://86046e34a0ff065c0440a0e57171c4d0c3bfdb57'
 # Specify device and os_version for testing
-caps['device'] = 'Google Pixel 3'
-caps['os_version'] = '9.0'
-
+caps['device'] = 'iPhone 15 Pro Max'
+caps['os_version'] = '17'
 # Set other BrowserStack capabilities
 caps['project'] = 'First Ruby project'
-caps['build'] = 'browserstack-build-1'
+caps['build'] = 'Ruby iOS'
 caps['name'] = 'single_test'
-
 #Set the platform name
-caps['platformName'] = 'android'
-
-# Initialize the remote Webdriver using BrowserStack remote URL
-# and desired capabilities defined above
+caps['platformName'] = 'iOS'
 appium_driver = Appium::Driver.new({
 	'caps' => caps,
 	'appium_lib' => {
-		:server_url => "http://hub-cloud.browserstack.com/wd/hub"
+		:server_url => "https://hub-cloud.browserstack.com/wd/hub"
 	}}, true)
 driver = appium_driver.start_driver
-
-# Test case for the BrowserStack sample Android app. 
-# If you have uploaded your app, update the test case here. 
-wait = Selenium::WebDriver::Wait.new(:timeout => 30)
-wait.until { driver.find_element(:accessibility_id, "Search Wikipedia").displayed? }
-element = driver.find_element(:accessibility_id, "Search Wikipedia")
-element.click
-
-wait.until { driver.find_element(:id, "org.wikipedia.alpha:id/search_src_text").displayed? }
-search_box = driver.find_element(:id, "org.wikipedia.alpha:id/search_src_text")
-search_box.send_keys("BrowserStack")
-sleep 5
-
-results = driver.find_elements(:class, "android.widget.TextView")
-if results.count > 0
-	puts "Found results - Test Passed"
-else
-	puts "No results found - Test Failed"
-end
-
-# Invoke driver.quit() after the test is done to indicate that the test is completed.
-driver.quit
